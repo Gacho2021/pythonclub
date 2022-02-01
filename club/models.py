b9022a4b403ac14e6713fb7193b35f6dd5e77197
Gacho2021 <87105994+Gacho2021@users.noreply.github.com>
2022-01-31 16:44:41 -0800

    Update models.py

--- club/models.py
+++ club/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class  TechType(models.model):
+    typename=models.CharField(max_length=255)
+    typedescription=models.TextFiled(null=true, blank=True)
+
+    def __str__(self):
+        return self.typename
+
+    class Meta:
+        db_table='techtype'
+
+class product(models.model):
+    productname=models.CharField(max_length=255)
+    producttype=models.ForignKey(TechType, on_delete=models.DO_NOTHING)
+    user=models.ForignKey(User, on_delete=models.DO_NOTHING)
+    dateentered=DateField()
+    price=models.DecimalField(max_digits='6', decimal_places=2)
+    productURL=models.UrlField(
+    description=models.TextField()
+
+    def __str__(self):
+        return self.productname
+
+        class Meta:
+            db_table='product'
+
+class Review(models.model):
+    title=models.CharField(max_length=255)
+    user=models.ForignKey(User, on_delete=models.CASCADE)
+    product=models.ForignKey(product, on_delete=models.CASCADE)
+    reviewdate=models.DateField()
+    reviewtext=models.TextField()
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        db_table='review'
