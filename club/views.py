26202c1266f2886e0e0c22963d593cd647b610cd
Gacho2021 <87105994+Gacho2021@users.noreply.github.com>
2022-02-07 15:54:29 -0800

    Update views.py

--- club/views.py
+++ club/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import product, techType, Review 
 
-# Create your views here.
+def index(request):
+    return render(request, 'club\index.html')
+
+    def products(request):
+        product_list=product.objects.all()
+        return render(request, 'club/product.html', {'product_list': product_list})
+ 
